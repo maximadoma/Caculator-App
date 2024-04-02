daa46f29bc96d8f4c5e4f546d7c9b6956ca3400e
maximadoma <fernylmaningo@gmail.com>
2024-04-02 14:14:23 +0800

    modified/updated

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -21,9 +21,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MaterialButton btn_0 = (MaterialButton) findViewById(R.id.zero);
-        btn_0.setOnClickListener(this);
+        numbers();
+        operators();
 
+        input = findViewById(R.id.textfield_bottom);
+        input.setShowSoftInputOnFocus(false);
+
+        input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (getString(R.string.display).equals(input.getText().toString())){
+                    input.setText("");
+                }
+            }
+        });
 
 
 
@@ -31,7 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public void Numbers(View v){
+    public void numbers(){
         MaterialButton btn_0 = (MaterialButton) findViewById(R.id.zero);
         MaterialButton btn_1 = (MaterialButton) findViewById(R.id.one);
         MaterialButton btn_2 = (MaterialButton) findViewById(R.id.two);
@@ -43,24 +54,153 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         MaterialButton btn_8 = (MaterialButton) findViewById(R.id.eight);
         MaterialButton btn_9 = (MaterialButton) findViewById(R.id.nine);
 
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+
+    }
+
+    public void operators(){
+        MaterialButton btn_plus = (MaterialButton) findViewById(R.id.addition);
+        MaterialButton btn_minus = (MaterialButton) findViewById(R.id.subtraction);
+        MaterialButton btn_multiply = (MaterialButton) findViewById(R.id.multiplication);
+        MaterialButton btn_divide = (MaterialButton) findViewById(R.id.division);
+
+        btn_plus.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
+
+    }
+
+
+    private void updateText(String strToAdd){
+        String oldString = input.getText().toString();
+        int cursorPosition = input.getSelectionStart();
+        String leftString = oldString.substring(0, cursorPosition);
+        String rightString = oldString.substring(cursorPosition);
+
+        input.setText(String.format("%s%s%s", leftString, strToAdd, rightString));
     }
 
 
     @Override
     public void onClick(View v) {
-       input = findViewById(R.id.textfield_bottom);
-//       number = input.setText();
 
 
         if (v.getId() == R.id.zero) {
-            number = 0;
-            input.setText(number);
+            zero_btn(v);
+
         }
         else if (v.getId() == R.id.one) {
-            input.setText("1");
+            one_btn(v);
         }
         else if (v.getId() == R.id.two) {
-            input.setText("2");
+            two_btn(v);
+        }
+        else if (v.getId() == R.id.three) {
+            three_btn(v);
+        }
+
+        else if (v.getId() == R.id.four) {
+            four_btn(v);
+        }
+
+        else if (v.getId() == R.id.five) {
+            five_btn(v);
+        }
+
+        else if (v.getId() == R.id.six) {
+            six_btn(v);
+        }
+
+        else if (v.getId() == R.id.seven) {
+            seven_btn(v);
+        }
+
+        else if (v.getId() == R.id.eight) {
+            eight_btn(v);
+        }
+
+        else if (v.getId() == R.id.nine) {
+            nine_btn(v);
+        }
+
+        else if (v.getId() == R.id.addition) {
+            plus_btn(v);
+        }
+        else if (v.getId() == R.id.subtraction) {
+            minus_btn(v);
+        }
+        else if (v.getId() == R.id.multiplication) {
+            multiply_btn(v);
+        }
+        else if (v.getId() == R.id.division) {
+            divide_btn(v);
         }
     }
+
+
+    public void zero_btn(View view){
+        updateText("0");
+    }
+
+    public void one_btn(View view){
+        updateText("1");
+    }
+
+    public void two_btn(View view){
+        updateText("2");
+    }
+
+    public void three_btn(View view){
+        updateText("3");
+    }
+
+    public void four_btn(View view){
+        updateText("4");
+    }
+
+    public void five_btn(View view){
+        updateText("5");
+    }
+
+    public void six_btn(View view){
+        updateText("6");
+    }
+
+    public void seven_btn(View view){
+        updateText("7");
+    }
+
+    public void eight_btn(View view){
+        updateText("8");
+    }
+    public void nine_btn(View view){
+        updateText("9");
+    }
+
+    public void plus_btn(View view){
+        updateText("+");
+    }
+
+    public void minus_btn(View view){
+        updateText("-");
+    }
+
+    public void multiply_btn(View view){
+        updateText("*");
+    }
+
+    public void divide_btn(View view){
+        updateText("/");
+    }
+
 }
