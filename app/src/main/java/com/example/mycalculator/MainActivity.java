055264cbde56ce8bef2b456161c618cbd1ff3927
maximadoma <fernylmaningo@gmail.com>
2024-04-02 18:21:15 +0800

    updated

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
+import org.mariuszgromada.math.mxparser.Expression;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -36,8 +38,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         });
-
-
 
 
     }
@@ -83,7 +83,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void misc_btn(){
         MaterialButton btn_allClear = (MaterialButton) findViewById(R.id.all_clear);
+        MaterialButton btn_backSpace = (MaterialButton) findViewById(R.id.delete);
+        MaterialButton btn_parenthesis = (MaterialButton) findViewById(R.id.parenthesis);
+        MaterialButton btn_dot = (MaterialButton) findViewById(R.id.dot);
+
         btn_allClear.setOnClickListener(this);
+        btn_backSpace.setOnClickListener(this);
+        btn_parenthesis.setOnClickListener(this);
+        btn_dot.setOnClickListener(this);
     }
 
 
@@ -93,7 +100,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String leftString = oldString.substring(0, cursorPosition);
         String rightString = oldString.substring(cursorPosition);
 
-        input.setText(String.format("%s%s%s", leftString, strToAdd, rightString));
+        if (getString(R.string.display).equals(input.getText().toString())){
+            input.setText(strToAdd);
+            input.setSelection(cursorPosition + 1);
+        }else{
+            input.setText(String.format("%s%s%s", leftString, strToAdd, rightString));
+            input.setSelection(cursorPosition + 1);
+        }
+
     }
 
 
@@ -154,9 +168,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         else if (v.getId() == R.id.all_clear) {
             allClear_btn(v);
         }
+        else if (v.getId() == R.id.delete) {
+            backSpace_btn(v);
+        }
+        else if (v.getId() == R.id.parenthesis) {
+            parenthesis_btn(v);
+        }
+        else if (v.getId() == R.id.dot) {
+            dot_btn(v);
+        }
+        else if (v.getId() == R.id.equals) {
+
+        }
     }
 
-
+    public void dot_btn(View view){
+        updateText(".");
+    }
     public void zero_btn(View view){
         updateText("0");
     }
@@ -213,7 +241,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void allClear_btn(View view){
-        updateText("");
+        input.setText("");
+    }
+
+
+    public void backSpace_btn(View view){
+        int cursorPosition = input.getSelectionStart();
+        int textLength = input.getText().length();
+
+        if (cursorPosition != 0 && textLength != 0){
+            SpannableStringBuilder selection = (SpannableStringBuilder) input.getText();
+            selection.replace(cursorPosition - 1, cursorPosition, "");
+            input.setText(selection);
+            input.setSelection(cursorPosition - 1);
+        }
+    }
+
+
+    public void parenthesis_btn(View view){
+        int cursorPosition = input.getSelectionStart();
+        int openParenthesis = 0;
+        int closeParenthesis = 0;
+        int textLength = input.getText().length();
+
+        for (int i = 0; i < cursorPosition; i++){
+            if (input.getText().toString().substring(i, i+1).equals("(")){
+                openParenthesis +=1;
+            }
+            if (input.getText().toString().substring(i, i+1).equals(")")){
+                closeParenthesis +=1;
+            }
+        }
+
+        if(openParenthesis == closeParenthesis || input.getText().toString().substring(textLength - 1, textLength).equals("(")){
+            updateText("(");
+        }
+
+        else if(closeParenthesis < openParenthesis && !input.getText().toString().substring(textLength - 1, textLength).equals("(")){
+            updateText(")");
+        }
+            input.setSelection(cursorPosition + 1);
+    }
+
+    public void equals_btn(View view){
+        String userExp = input.getText().toString();
+
+        userExp = userExp.replaceAll("÷", "/");
+        userExp = userExp.replaceAll("×", "*");
+
+        Expression exp = new Expression(userExp);
     }
 
 }
